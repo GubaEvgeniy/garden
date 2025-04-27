bb5b0e0151601b33a70510d15f94c5bea423ff2c
e.guba <e.guba@platform.live>
2025-04-27 12:05:56 +0200

    claude:5:delete unused

--- lib/domain/entities/garden.dart
+++
@@ -1,8 +0,0 @@
-import 'package:garden_of_feelings/domain/entities/plant.dart';
-
-class Garden {
-  final List<Plant?> grid;
-  // Можно включить также размеры, уровень воды, т.д.
-
-  Garden({required this.grid});
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:garden_of_feelings/presentation/screens/animated_garden_screen1.dart';
-import 'presentation/screens/garden_screen.dart';
 
 void main() {
   runApp(const GardenApp());
@@ -11,13 +9,6 @@ class GardenApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Мой Сад',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const AnimatedGardenScreen1(),
-    );
+    return SizedBox();
   }
 }
